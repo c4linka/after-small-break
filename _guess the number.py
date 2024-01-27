455aea2c1b3a8c78f10a90de98c70888afd27175
Ewa <155647516+c4linka@users.noreply.github.com>
2024-01-27 16:53:56 +0100

    Add files via upload

---
+++ _guess the number.py
@@ -0,0 +1,37 @@
+#nadom number
+
+import random    
+theNumber = random.randint(1,10)
+print("Hello there :) Let's play. Can you guess the number on my mind?")
+guessNumber = int(input("Give it a try, what number I am thinking about? "))
+
+
+while guessNumber != theNumber:
+    print("Nope, the number is different")
+    if guessNumber > theNumber:
+        print("The number is smaller than that")
+    elif guessNumber < theNumber:
+        print("The number is bigger than that")
+    guessNumber = int(input("Try again "))
+
+print("Amazing! You guessed the number! It is ",theNumber," indeed!")
+
+
+#selected number
+  
+theNumber = 5
+print("Hello there :) Let's play. Can you guess the number on my mind?")
+guessNumber = int(input("Give it a try, what number I am thinking about? "))
+
+while guessNumber != theNumber:
+    print("Nope, the number is different")
+    if guessNumber > theNumber:
+        print("The number is smaller than that")
+    elif guessNumber < theNumber:
+        print("The number is bigger than that")
+    guessNumber = int(input("Try again: "))
+
+print("Amazing! You guessed the number! It is",theNumber,"indeed!")
+
+
+
